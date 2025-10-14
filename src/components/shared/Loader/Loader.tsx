3d4e66e08fc263d5af787128a64cea1396527480
Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-10-15 00:37:10 +0600

    loader added

---
+++ src/components/shared/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import { FadeLoader } from 'react-spinners';
+
+const Loader = () => {
+    return (
+        <div>
+            <FadeLoader />
+        </div>
+    );
+};
+
+export default Loader;
