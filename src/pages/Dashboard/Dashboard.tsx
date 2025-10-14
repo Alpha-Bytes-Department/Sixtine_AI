ba43de1a4e79ed289bfec8e38cd93e5c3eced452
Joy Sutradhar <joysd.1005@fec.edu.bd>
2025-10-15 03:07:28 +0600

    Enhance SideNav with mobile navigation and search functionality; update Dashboard layout

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
   const setNewPassword = useMatch("/dashboard/set-new-password");
   const passwordResetSuccessful = useMatch("/dashboard/password-update/successful");
   return (
-    <div className="flex w-full h-screen bg-white">
+    <div className="flex lg:flex-row flex-col w-full h-screen bg-white">
       {isLogIn || forgotPassword || OtpVerification || setNewPassword || passwordResetSuccessful ? <></> : <SideNav />}
       <Outlet />
     </div>
