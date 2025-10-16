0ef1b444cca13408f250cf72e8410f7229ef62b9
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-10-16 20:52:06 +0600

    fix some bugges

--- src/pages/EditProfile/EditProfile.tsx
+++ src/pages/EditProfile/EditProfile.tsx
@@ -1,26 +1,37 @@
+import { useRef } from "react";
+import { FaCamera } from "react-icons/fa";
 import { RxCross2 } from "react-icons/rx";
 import { Link } from "react-router";
-import { FaCamera } from "react-icons/fa";
 
 const EditProfile = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleSelectImage = () => {
+    inputRef.current?.click();
+  };
+
   return (
-    <div className="flex-1 flex justify-center items-center">
-      <div className="shadow-[0_0_4px_0_#00000040] p-4 sm:p-8 md:p-12 lg:p-16 xl:p-20 relative w-full max-w-2xl mx-auto">
+    <div className="flex-1 flex justify-center items-center overflow-y-auto xl:py-20 2xl:py-0">
+      <div className="shadow-[0_0_4px_0_#00000040] p-4 sm:p-8 md:p-12 lg:p-16 xl:px-16 xl:py-3  2xl:p-20 relative w-full max-w-2xl mx-auto">
         <Link to={"/dashboard/profile"}>
           <RxCross2 className="absolute top-5 right-5 text-xl text-[#4E7BA0]" />
         </Link>
         <div className="flex flex-col gap-5 text-[#4A4A4A] ">
-          <h1 className="text-2xl sm:text-3xl font-semibold">Edit Profile</h1>
+          <h1 className="text-2xl 2xl:text-3xl font-semibold">Edit Profile</h1>
           <div className="relative self-center">
             <img
               src="/Images/Others/demoProfile.jpg"
               alt="profile"
               className="h-24 w-24 rounded-full"
             />
-            <p className="absolute bottom-0 left-16 bg-white rounded-full p-2 cursor-pointer">
+            <p
+              onClick={() => handleSelectImage()}
+              className="absolute bottom-0 left-16 bg-white rounded-full p-2 cursor-pointer"
+            >
               <FaCamera className="text-[#4E7BA0]" />
             </p>
           </div>
+          <input type="file" ref={inputRef} className="hidden" />
           <div className="flex flex-col sm:flex-row items-start sm:items-center gap-2 sm:gap-12">
             <span className="w-full sm:w-40 font-semibold">First name</span>
             <input
@@ -75,16 +86,16 @@ const EditProfile = () => {
             <span className="w-full sm:w-40 font-semibold">Role</span>
             <div className="relative w-full">
               <select
-              name="role"
-              id="role"
-              className="w-full bg-[#EAEAEA] text-gray-700 px-3 py-2 rounded-sm outline-none cursor-pointer appearance-none border-none [&>option]:bg-[#EAEAEA] [&>option]:border-none [&>option]:rounded-sm [&>option]:text-gray-700 [&>option]:p-2"
+                name="role"
+                id="role"
+                className="w-full bg-[#EAEAEA] text-gray-700 px-3 py-2 rounded-sm outline-none cursor-pointer appearance-none border-none [&>option]:bg-[#EAEAEA] [&>option]:border-none [&>option]:rounded-sm [&>option]:text-gray-700 [&>option]:p-2"
               >
-              <option value="" disabled selected hidden>
-                Select Role
-              </option>
-              <option value="doctor">Doctor</option>
-              <option value="nurse">Nurse</option>
-              <option value="medical assistant">Medical Assistant</option>
+                <option value="" disabled selected hidden>
+                  Select Role
+                </option>
+                <option value="doctor">Doctor</option>
+                <option value="nurse">Nurse</option>
+                <option value="medical assistant">Medical Assistant</option>
               </select>
             </div>
           </div>
